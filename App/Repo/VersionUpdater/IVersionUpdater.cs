8c69ff86c3239439b5056a1c866d971116535969
StarKRE <ardar.igor@mail.ru>
2020-09-14 16:17:47 +0300

    Added doc for repo layer

--- App/Repo/VersionUpdater/IVersionUpdater.cs
+++ App/Repo/VersionUpdater/IVersionUpdater.cs
@@ -2,8 +2,14 @@ using System.Collections;
 
 namespace OregoFramework.App
 {
+    /// <summary>
+    ///     <para>Checks and updates a user data.</para>
+    /// </summary>
     public interface IVersionUpdater : IRepoElement
     {
+        /// <summary>
+        ///     <para>Checks and updates a user data asynchronously.</para>
+        /// </summary>
         IEnumerator CheckForUpdates();
     }
 }
--- App/Repo/VersionUpdater/VersionUpdater.cs
+++ App/Repo/VersionUpdater/VersionUpdater.cs
@@ -2,8 +2,10 @@ using System.Collections;
 
 namespace OregoFramework.App
 {
+    /// <inheritdoc cref="IVersionUpdater"/>
     public abstract class VersionUpdater : RepoElement, IVersionUpdater
     {
+        /// <inheritdoc cref="IVersionUpdater.CheckForUpdates"/>
         public abstract IEnumerator CheckForUpdates();
     }
 }
