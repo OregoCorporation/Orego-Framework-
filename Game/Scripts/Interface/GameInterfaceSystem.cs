c2efbfbfd56255b96cd2beb3febb13764a7c4fab
igor_gulkin <ardar.igor@mail.ru>
2020-10-31 17:22:09 +0700

    Added doc to game module

--- Game/Scripts/Interface/GameInterfaceSystem.cs
+++ Game/Scripts/Interface/GameInterfaceSystem.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 
 namespace OregoFramework.Game
 {
+    /// <inheritdoc cref="IGameInterfaceSystem"/>
     public abstract class GameInterfaceSystem : MonoBehaviour, IGameInterfaceSystem
     {
+        /// <para>Bound game context reference.</para>
         public IGameContext gameContext { get; private set; }
 
         protected IEnumerable<IGameInterface> registeredInterfaces
@@ -14,6 +16,9 @@ namespace OregoFramework.Game
             get { return this.registeredInterfaceMap.Values; }
         }
 
+        /// <summary>
+        ///     <para>Map of registered unique interfaces.</para>
+        /// </summary>
         private readonly Dictionary<Type, IGameInterface> registeredInterfaceMap;
 
         protected GameInterfaceSystem()
@@ -21,32 +26,45 @@ namespace OregoFramework.Game
             this.registeredInterfaceMap = new Dictionary<Type, IGameInterface>();
         }
 
-        public virtual void RegisterInterface(IGameInterface gameInterface)
+        /// <inheritdoc cref="IGameInterfaceSystem.RegisterInterface"/>
+        public void RegisterInterface(IGameInterface gameInterface)
         {
             var type = gameInterface.GetType();
             this.registeredInterfaceMap.Add(type, gameInterface);
             gameInterface.OnRegistered(this);
         }
 
-        public virtual void UnregisterInterface(IGameInterface gameInterface)
+        /// <inheritdoc cref="IGameInterfaceSystem.UnregisterInterface"/>
+        public void UnregisterInterface(IGameInterface gameInterface)
         {
             gameInterface.OnUnregistered();
             var type = gameInterface.GetType();
             this.registeredInterfaceMap.Remove(type);
         }
 
+        /// <inheritdoc cref="IGameInterfaceSystem.GetInterface{T}"/>
         public T GetInterface<T>() where T : IGameInterface
         {
             return this.registeredInterfaceMap.Find<T, IGameInterface>();
         }
 
+        /// <inheritdoc cref="IGameInterfaceSystem.GetInterfaces{T}"/>
         public IEnumerable<T> GetInterfaces<T>() where T : IGameInterface
         {
             return this.registeredInterfaceMap.FindAll<T, IGameInterface>();
         }
 
-        public virtual void BindGameContext(IGameContext gameContext)
+        /// <summary>
+        ///     <para>Binds game context.</para>
+        /// </summary>
+        /// <param name="gameContext">Target game context.</param>
+        public void BindGameContext(IGameContext gameContext)
         {
+            if (this.gameContext != null)
+            {
+                throw new Exception("Game context has already bound!");
+            }
+
             this.gameContext = gameContext;
             this.gameContext.OnGamePreparedEvent += this.OnGamePrepared;
             this.gameContext.OnGameReadyEvent += this.OnGameReady;
@@ -56,8 +74,17 @@ namespace OregoFramework.Game
             this.gameContext.OnGameFinishedEvent += this.OnGameFinished;
         }
 
-        public virtual void UnbindGameContext()
+        /// <summary>
+        ///     <para>Unbinds game context.</para>
+        /// </summary>
+        /// <param name="gameContext">Target game context.</param>
+        public void UnbindGameContext()
         {
+            if (this.gameContext is null)
+            {
+                return;
+            }
+
             this.gameContext.OnGamePreparedEvent -= this.OnGamePrepared;
             this.gameContext.OnGameReadyEvent -= this.OnGameReady;
             this.gameContext.OnGameStartedEvent -= this.OnGameStarted;
@@ -69,90 +96,97 @@ namespace OregoFramework.Game
 
         #region GameCallbacks
 
+        /// <inheritdoc cref="IGameContext.PrepareGame"/>
         private void OnGamePrepared(object sender)
         {
-            foreach (var view in this.registeredInterfaces)
+            foreach (var gameInterface in this.registeredInterfaces)
             {
-                view.OnGamePrepared(sender);
+                gameInterface.OnGamePrepared(sender);
             }
-            
+
             this.OnGamePrepared(this, sender);
         }
 
-        private void OnGameReady(object sender)
-        {
-            foreach (var view in this.registeredInterfaces)
-            {
-                view.OnGameReady(sender);
-            }
-            
-            this.OnGameReady(this, sender);
-        }
-
-        private void OnGameStarted(object sender)
-        {
-            foreach (var view in this.registeredInterfaces)
-            {
-                view.OnGameStarted(sender);
-            }
-            
-            this.OnGameStarted(this, sender);
-        }
-
-        private void OnGamePaused(object sender)
-        {
-            foreach (var view in this.registeredInterfaces)
-            {
-                view.OnGamePaused(sender);
-            }
-            
-            this.OnGamePaused(this, sender);
-        }
-
-        private void OnGameResumed(object sender)
-        {
-            foreach (var view in this.registeredInterfaces)
-            {
-                view.OnGameResumed(sender);
-            }
-            
-            this.OnGameResumed(this, sender);
-        }
-
-        private void OnGameFinished(object sender)
-        {
-            foreach (var view in this.registeredInterfaces)
-            {
-                view.OnGameFinished(sender);
-            }
-            
-            this.OnGameFinished(this, sender);
-        }
-
-        #endregion
-
         protected virtual void OnGamePrepared(GameInterfaceSystem _, object sender)
         {
+        }
+
+
+        /// <inheritdoc cref="IGameContext.ReadyGame"/>
+        private void OnGameReady(object sender)
+        {
+            foreach (var gameInterface in this.registeredInterfaces)
+            {
+                gameInterface.OnGameReady(sender);
+            }
+
+            this.OnGameReady(this, sender);
         }
 
         protected virtual void OnGameReady(GameInterfaceSystem _, object sender)
         {
         }
 
+        /// <inheritdoc cref="IGameContext.StartGame"/>
+        private void OnGameStarted(object sender)
+        {
+            foreach (var gameInterface in this.registeredInterfaces)
+            {
+                gameInterface.OnGameStarted(sender);
+            }
+
+            this.OnGameStarted(this, sender);
+        }
+
         protected virtual void OnGameStarted(GameInterfaceSystem _, object sender)
         {
+        }
+
+        /// <inheritdoc cref="IGameContext.PauseGame"/>
+        private void OnGamePaused(object sender)
+        {
+            foreach (var gameInterface in this.registeredInterfaces)
+            {
+                gameInterface.OnGamePaused(sender);
+            }
+
+            this.OnGamePaused(this, sender);
         }
 
         protected virtual void OnGamePaused(GameInterfaceSystem _, object sender)
         {
         }
 
+        /// <inheritdoc cref="IGameContext.ResumeGame"/>
+        private void OnGameResumed(object sender)
+        {
+            foreach (var gameInterface in this.registeredInterfaces)
+            {
+                gameInterface.OnGameResumed(sender);
+            }
+
+            this.OnGameResumed(this, sender);
+        }
+
         protected virtual void OnGameResumed(GameInterfaceSystem _, object sender)
         {
+        }
+
+        /// <inheritdoc cref="IGameContext.FinishGame"/>
+        private void OnGameFinished(object sender)
+        {
+            foreach (var gameInterface in this.registeredInterfaces)
+            {
+                gameInterface.OnGameFinished(sender);
+            }
+
+            this.OnGameFinished(this, sender);
         }
 
         protected virtual void OnGameFinished(GameInterfaceSystem _, object sender)
         {
         }
+
+        #endregion
     }
 }
